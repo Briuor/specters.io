21e57c0f5cfd41c9880886130c327488f59dcaf5
Briuor <briuorlol89@gmail.com>
2020-11-11 16:37:14 -0300

    fix shoot angle

--- server/models/player.js
+++ server/models/player.js
@@ -1,14 +1,14 @@
-// const Matter = require('matter-js');
+const Matter = require('matter-js');
 const Bullet = require("./bullet");
 
 class Player {
     constructor(x, y) {
         this.hp = 100;
-        // this.body = Matter.Bodies.circle(x, y, 20, [], 50);
+        // this.body = Matter.Bodies.circle(x, y, 24, [], 50);
         // Matter.Body.setAngle(this.body, 0);
         this.x = x;
         this.y = y;
-        this.r = 64;
+        this.r = 24;
         this.direction = { right: false, left: false, up: false, down: false };
         this.speed = 5;
         this.color = 'red';
@@ -42,8 +42,8 @@ class Player {
     }
 
     updateAngle(input) {
-        let centerX = input.screen.x + this.r/2;
-        let centerY = input.screen.y + this.r/2;
+        let centerX = input.screen.x;
+        let centerY = input.screen.y;
 
         this.angle = Math.atan2(input.y - centerY, input.x - centerX) + Math.PI / 2;
     }
